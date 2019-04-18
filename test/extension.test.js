4ad05829dfeb23f2e918f815b3d45550780c1496
Brent Mealhouse <bmealhouse@gmail.com>
2019-04-18 15:42:16 -0500

    Test global extension features

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,135 +1,40 @@
-// const assert = require('assert')
-// const shifty = require('../src/extension')
-// const codefaceFontFamilies = require('../src/font-families/codeface-font-families')
-// const macosFontFamilies = require('../src/font-families/macos-font-families')
-// const windowsFontFamilies = require('../src/font-families/windows-font-families')
+const assert = require('assert')
+const vscode = require('vscode')
+const {getCurrentColorTheme, setColorTheme} = require('../src/color-themes')
+const {getCurrentFontFamily, setFontFamily} = require('../src/font-families')
 
-// setup(async () => {
-//   originalColorTheme = shifty.getCurrentColorTheme()
-//   await shifty.setColorTheme('Visual Studio Dark')
+let originalColorTheme = null
+let originalFontFamily = null
+const DEFAULT_COLOR_THEME = 'Visual Studio Dark'
+const DEFUALT_FONT_FAMILY = 'Monaco'
 
-//   originalFontFamily = shifty.getCurrentFontFamily()
-//   await shifty.setFontFamily('Monaco')
-// })
+setup(async () => {
+  originalColorTheme = getCurrentColorTheme()
+  await setColorTheme(DEFAULT_COLOR_THEME)
 
-// teardown(async () => {
-//   shifty.deactivate()
+  originalFontFamily = getCurrentFontFamily()
+  setFontFamily(DEFUALT_FONT_FAMILY)
+})
 
-//   if (originalColorTheme) {
-//     await shifty.setColorTheme(originalColorTheme)
-//   }
+teardown(async () => {
+  if (originalColorTheme) {
+    await setColorTheme(originalColorTheme)
+  }
 
-//   if (originalColorTheme) {
-//     await shifty.setFontFamily(originalFontFamily)
-//   }
-// })
+  if (originalColorTheme) {
+    await setFontFamily(originalFontFamily)
+  }
+})
 
-// suite('shifty', function() {
-//   test('should shift color theme on startup', async () => {
-//     const config = getShiftyConfig({
-//       startup: {
-//         shiftColorThemeOnStartup: true,
-//       },
-//     })
+suite('extension.test.js', () => {
+  test('should register global commands when VS Code starts up', async () => {
+    const commands = await vscode.commands.getCommands()
+    assert.ok(commands.includes('shifty.shiftAll'))
+  })
 
-//     const currentColorTheme = shifty.getCurrentColorTheme()
-//     await shifty.initializeExtension(config)
-//     const nextColorTheme = shifty.getCurrentColorTheme()
-
-//     assert.strict.notEqual(nextColorTheme, currentColorTheme)
-//   })
-
-//   test('should not shift color theme on startup when disabled', async () => {
-//     const config = getShiftyConfig({
-//       startup: {
-//         shiftColorThemeOnStartup: false,
-//       },
-//     })
-
-//     const currentColorTheme = shifty.getCurrentColorTheme()
-//     await shifty.initializeExtension(config)
-//     const nextColorTheme = shifty.getCurrentColorTheme()
-
-//     assert.equal(nextColorTheme, currentColorTheme)
-//   })
-
-//   test('should shift font family on startup', async () => {
-//     const config = getShiftyConfig({
-//       startup: {
-//         shiftFontFamilyOnStartup: true,
-//       },
-//     })
-
-//     const currentFontFamily = shifty.getCurrentFontFamily()
-//     await shifty.initializeExtension(config)
-//     const nextFontFamily = shifty.getCurrentFontFamily()
-
-//     assert.notEqual(nextFontFamily, currentFontFamily)
-//   })
-
-//   test('should not shift font family on startup when disabled', async () => {
-//     const config = getShiftyConfig({
-//       startup: {
-//         shiftFontFamilyOnStartup: false,
-//       },
-//     })
-
-//     const currentFontFamily = shifty.getCurrentFontFamily()
-//     await shifty.initializeExtension(config)
-//     const nextFontFamily = shifty.getCurrentFontFamily()
-
-//     assert.equal(nextFontFamily, currentFontFamily)
-//   })
-
-//   test('should shift color theme using an 25ms interval', function(done) {
-//     this.timeout(250)
-
-//     const config = getShiftyConfig({
-//       shiftInterval: {
-//         enabled: true,
-//         shiftColorThemeIntervalMs: 10,
-//       },
-//     })
-
-//     const currentColorTheme = shifty.getCurrentColorTheme()
-
-//     shifty.initializeExtension(config).then(() => {
-//       function waitForColorThemeToShift() {
-//         if (currentColorTheme !== shifty.getCurrentColorTheme()) {
-//           done()
-//         } else {
-//           setTimeout(waitForColorThemeToShift, 10)
-//         }
-//       }
-//       waitForColorThemeToShift()
-//     })
-//   })
-
-//   test('should not shift color theme using an interval when disabled', function(done) {
-//     this.timeout(250)
-
-//     const config = getShiftyConfig({
-//       shiftInterval: {
-//         enabled: false,
-//         shiftColorThemeIntervalMs: 10,
-//       },
-//     })
-
-//     const currentColorTheme = shifty.getCurrentColorTheme()
-
-//     shifty.initializeExtension(config).then(() => {
-//       function waitForColorThemeToShift() {
-//         if (currentColorTheme !== shifty.getCurrentColorTheme()) {
-//           assert.fail()
-//         } else {
-//           setTimeout(waitForColorThemeToShift, 10)
-//         }
-//       }
-//       waitForColorThemeToShift()
-//     })
-//   })
-
-//   test('should shift font family using an 100ms interval')
-
-//   test('should not shift font family using an interval when disabled')
-// })
+  test('should shift the color theme and font family when running the "shifty.shiftAll" command', async () => {
+    await vscode.commands.executeCommand('shifty.shiftAll')
+    assert.notStrictEqual(getCurrentColorTheme(), DEFAULT_COLOR_THEME)
+    assert.notStrictEqual(getCurrentFontFamily(), DEFUALT_FONT_FAMILY)
+  })
+})
